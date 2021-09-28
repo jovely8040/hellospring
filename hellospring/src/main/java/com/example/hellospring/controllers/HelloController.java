9aba8e2a0229ce1b1393ef32eacdcc3e86399727
조은혜 <jovely8040@gmail.com>
2021-09-28 12:56:20 +0900

    메서드 단독 매핑

--- hellospring/src/main/java/com/example/hellospring/controllers/HelloController.java
+++ hellospring/src/main/java/com/example/hellospring/controllers/HelloController.java
@@ -3,8 +3,10 @@ package com.example.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+//	메서드 단독 매핑
 //@Controller("myController")
 @Controller
 public class HelloController {
@@ -14,7 +16,7 @@ public class HelloController {
 		//	서블릿에서 getParameter로 해준 것을
 		//	 RequestParam이 대신 해준 것
 		ModelAndView mav = new ModelAndView();
-
+		
 		//	객체 추가: 서블릿에서 addAttribute의 역할
 		mav.addObject("message", 	//	키
 				"Hello, " + name);	//	값
@@ -22,5 +24,19 @@ public class HelloController {
 		mav.setViewName("/WEB-INF/views/hello.jsp");
 
 		return mav;
+	}
+
+	//	문자열을 반환하면 해당 뷰로의 포워딩을 의미
+	@RequestMapping("/hello2")
+	public String hello2() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	//	문자열을 반환할 때 직접 응답으로 전송하고 싶을 때
+	//	ResponseBody -> MessageConverter를 실행하는 어노테이션
+	@ResponseBody	
+	@RequestMapping("/hello3")
+	public String hello3() {
+		return "<h1>문자열 직접 전송</h1>";
 	}
 }
